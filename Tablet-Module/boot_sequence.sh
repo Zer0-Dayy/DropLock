c4ae5953e609607077180f253bb6242932a0ad20
Zer0-Dayy <221895146+Zer0-Dayy@users.noreply.github.com>
2026-05-21 19:57:52 +0100

    Added an ESP32-CAM for surveillance.

--- Tablet-Module/boot_sequence.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-source /home/zero-day/Projects/DropLock/Tablet-Module/env.sh
-cd /home/zero-day/Projects/DropLock/Tablet-Module
-
-exec /home/zero-day/Projects/DropLock/Tablet-Module/.venv/bin/python /home/zero-day/Projects/DropLock/Tablet-Module/main.py
